d81ef7c22a4105eeac24d4ed9577ade0766a38c4
redjun89 <jjune1816@gmail.com>
2023-04-17 19:36:10 +0900

    npm init 및 express, mongoose 설치, app.js 파일 코드 수정

--- app.js
+++ app.js
@@ -1,36 +1,10 @@
-// Express 초기화 및 미들웨어 등록
-
 const express = require("express");
-const mongoose = require('mongoose');
-const port = process.env.PORT || 3000;
-const db = mongoose.connection;
 const app = express();
-//express 모듈을 불러와서 애플리케이션 객체 생성
-
-const bodyParser = require("body-parser");
-const morgan = require("morgan");
-//body-parser를 사용하여 POST 요청으로 전송된 JSON 데이터 파싱
+const port = 3000;
 
 const routes = require("./routes");
 
-mongoose.connect('mongodb://localhost:27017/myapp', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true
-});
-
-
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log('Connected to MongoDB');
-});
-//mongoose를 이용해 MongoDB와 연결
-
-
-app.use(bodyParser.json());
-app.use(morgan("tiny"));
-
+app.use(express.json());
 app.use("/", routes);
 
 app.listen(port, () => {
