aa7396245f7476ca869774bb0b4979367c108ceb
redjun89 <jjune1816@gmail.com>
2023-04-18 13:34:09 +0900

    api 명세에 따라 코드 수정

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,12 @@
 const express = require("express");
 const router = express.Router();
 
-const posts = require("./posts");
-const comments = require("./comments");
+const posts = require("./posts.js");
+const comments = require("./comments.js");
 // const connect = require("./schemas");
 // connect();
 
-router.use("/posts", posts);
-router.use("/comments", comments);
+router.use("/", [posts, comments]);
+// router.use("/comments", comments);
 
 module.exports = router;
