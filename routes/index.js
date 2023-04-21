4eb24a1dd998b51317209a2a753d1924eb1912b3
redjun89 <jjune1816@gmail.com>
2023-04-21 10:17:00 +0900

    회원가입 route, schema 코드 초기 세팅

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,11 @@
 const express = require("express");
 const router = express.Router();
 
-const posts = require("./posts.js");
-const comments = require("./comments.js");
-// const connect = require("./schemas");
-// connect();
+const posts = require("./posts");
+const comments = require("./comments");
+const users = require("./users");
 
-router.use("/", [posts, comments]);
-// router.use("/comments", comments);
+
+router.use("/", [posts, comments, users]);
 
 module.exports = router;
