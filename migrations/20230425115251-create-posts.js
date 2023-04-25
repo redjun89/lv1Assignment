98c8d3d86ff72b450ec577e4536884d8a6ddb942
redjun89 <jjune1816@gmail.com>
2023-04-25 22:06:45 +0900

    posts model, migration 수정 완료

--- migrations/20230425115251-create-posts.js
+++ migrations/20230425115251-create-posts.js
@@ -3,28 +3,42 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Posts', {
-      id: {
+      postId: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
       UserId: {
-        type: Sequelize.INTEGER
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: { // 참조 설정
+          model: 'Users', // 참조하는 모델 설정
+          key: 'userId',  // 참조하는 모델의 column 설정
+        },
+        onDelete: 'CASCADE'  // 만약 Users 모델의 userId가 삭제되면 데이터 posts의 데이터도 삭제
+      },
+      nickname: {
+        allowNull: false,
+        type: Sequelize.STRING
       },
       title: {
+        allowNull: false,
         type: Sequelize.STRING
       },
       content: {
+        allowNull: false,
         type: Sequelize.STRING
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn("now")
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn("now")
       }
     });
   },
