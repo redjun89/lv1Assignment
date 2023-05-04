f769e236cc4fd3eada5a847ae64f81abd1e81b0b
redjun89 <jjune1816@gmail.com>
2023-05-04 16:34:29 +0900

    post 계층 분리

--- controllers/posts.controller.js
+++ controllers/posts.controller.js
@@ -1,26 +1,55 @@
-// controllers/posts.controller.js
-
 const PostService = require('../services/posts.service');
 
-// Post의 컨트롤러(Controller)역할을 하는 클래스
 class PostsController {
-    postService = new PostService(); // Post 서비스를 클래스를 컨트롤러 클래스의 멤버 변수로 할당합니다.
+  postService = new PostService();
 
-    getPosts = async (req, res, next) => {
-        // 서비스 계층에 구현된 findAllPost 로직을 실행합니다.
-        const posts = await this.postService.findAllPost();
+  getPosts = async (req, res, next) => {
+    const posts = await this.postService.findAllPost();
 
-        res.status(200).json({ data: posts })
-    }
+    res.status(200).json({ data: posts });
+  };
 
-    createPost = async (req, res, next) => {
-        const { nickname, password, title, content } = req.body;
+  getPostById = async (req, res, next) => {
+    const { postId } = req.params;
+    const post = await this.postService.findPostById(postId);
 
-        // 서비스 계층에 구현된 createPost 로직을 실행합니다.
-        const createPostData = await this.postService.createPost(nickname, password, title, content);
+    res.status(200).json({ data: post });
+  };
 
-        res.status(201).json({ data: createPostData });
-    }
+  createPost = async (req, res, next) => {
+    const { title, content, createdAt, updatedAt } = req.body;
+    const createPostData = await this.postService.createPost(
+      nickname,
+      password,
+      title,
+      content
+    );
+
+    res.status(201).json({ data: createPostData });
+  };
+
+  updatePost = async (req, res, next) => {
+    const { postId } = req.params;
+    const { password, title, content } = req.body;
+
+    const updatePost = await this.postService.updatePost(
+      postId,
+      password,
+      title,
+      content
+    );
+
+    res.status(200).json({ data: updatePost });
+  };
+
+  deletePost = async (req, res, next) => {
+    const { postId } = req.params;
+    const { password } = req.body;
+
+    const deletePost = await this.postService.deletePost(postId, password);
+
+    res.status(200).json({ data: deletePost });
+  };
 }
 
 module.exports = PostsController;
--- repositories/posts.repository.js
+++ repositories/posts.repository.js
@@ -2,18 +2,42 @@ const { Posts } = require('../models');
 
 class PostRepository {
   findAllPost = async () => {
-    // ORM인 Sequelize에서 Posts 모델의 findAll 메소드를 사용해 데이터를 요청합니다.
     const posts = await Posts.findAll();
 
     return posts;
-  }
+  };
+
+  findPostById = async (postId) => {
+    const post = await Posts.findByPk(postId);
+
+    return post;
+  };
 
   createPost = async (nickname, password, title, content) => {
-    // ORM인 Sequelize에서 Posts 모델의 create 메소드를 사용해 데이터를 요청합니다.
-    const createPostData = await Posts.create({ nickname, password, title, content });
+    const createPostData = await Posts.create({
+      nickname,
+      password,
+      title,
+      content,
+    });
 
     return createPostData;
-  }
+  };
+
+  updatePost = async (postId, password, title, content) => {
+    const updatePostData = await Posts.update(
+      { title, content },
+      { where: { postId, password } }
+    );
+
+    return updatePostData;
+  };
+
+  deletePost = async (postId, password) => {
+    const updatePostData = await Posts.destroy({ where: { postId, password } });
+
+    return updatePostData;
+  };
 }
 
 module.exports = PostRepository;
