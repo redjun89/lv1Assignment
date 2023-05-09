ba78dae4d0d1cedfd753c3ba55ae043343f25070
redjun89 <jjune1816@gmail.com>
2023-05-09 21:38:23 +0900

    dotenv 적용

--- config/config.js
+++ config/config.js
@@ -1,23 +1,27 @@
-{
-  "development" = {
+require("dotenv").config();
+
+  const development = {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASSWORD,
     "database": "Lv3_assignment",
     "host": process.env.DB_HOST,
     "dialect": "mysql"
-  },
-  "test" = {
+  };
+
+  const test = {
     "username": "root",
     "password": null,
     "database": "database_test",
     "host": "127.0.0.1",
     "dialect": "mysql"
-  },
-  "production" = {
+  };
+
+  const production = {
     "username": "root",
     "password": null,
     "database": "database_production",
     "host": "127.0.0.1",
     "dialect": "mysql"
-  }
-}
+  };
+
+module.exports = { development, test, production };
